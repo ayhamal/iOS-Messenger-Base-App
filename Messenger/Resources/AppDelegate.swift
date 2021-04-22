af0b9a8c47f84f0a59342822d52759d375ee508f
user191300 <user191300@NY101.local>
2021-04-22 10:31:37 -0400

    Added pod dependencies

--- Messenger/Resources/AppDelegate.swift
+++ Messenger/Resources/AppDelegate.swift
@@ -6,14 +6,14 @@
 //
 
 import UIKit
-//import Firebase
+import Firebase
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // MARK: Firebase Init
-        // FirebaseApp.configure()
+        FirebaseApp.configure()
         // Override point for customization after application launch.
         return true
     }
